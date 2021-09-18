84d54f13f82f1b3c36c6e4a9dab2afa8fe737857
Md. Al-Imran <asq24i@gmail.com>
2021-09-18 11:58:28 +0600

    login logout works

--- AuthSystem/urls.py
+++ AuthSystem/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', login, name="login"),
+    path('', login_view, name="login"),
     path('home/', homeview, name="home"),
+    path('studenthome/', homeview, name="student-home"),
     path('logout/', logout_view, name = "logout"),
 ]
--- AuthSystem/views.py
+++ AuthSystem/views.py
@@ -4,19 +4,22 @@ from .forms import UserLoginForm
 from django.contrib.auth import authenticate, login, logout
 
 
+
 # Create your views here.
 
-def login(request):
+def login_view(request):
     next = request.GET.get('next')
     form = UserLoginForm(request.POST or None)
     if form.is_valid():
         email = form.cleaned_data.get('email')
         password = form.cleaned_data.get('password')
         user = authenticate(email=email, password=password)
+        user_type = request.user
+        print("---------------------------", user_type)
         login(request, user)
         if next:
             return redirect(next)
-        return redirect('home/')
+        return redirect('studenthome/')
 
     context = {
         'login_form': form,
@@ -24,8 +27,11 @@ def login(request):
     return render(request, "login.html", context)
 
 def homeview(request):
-    return render(request, "home.html")
+    return render(request, "studenthome.html")
 
 def logout_view(request):
     logout(request)
-    return redirect('login/')
+    return redirect('/')
+
+def admin_view(request):
+    return redirect('admin/')
