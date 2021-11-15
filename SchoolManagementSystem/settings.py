d9d411e6afcda41328ac38be57056835b16a737e
Md Al Imran <48824546+imraan024@users.noreply.github.com>
2021-11-16 02:28:22 +0600

    Pass updated

--- SchoolManagementSystem/settings.py
+++ SchoolManagementSystem/settings.py
@@ -145,4 +145,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'alimraan24@gmail.com'
-EMAIL_HOST_PASSWORD = 'tinnimran2434'
+EMAIL_HOST_PASSWORD = 'password'
